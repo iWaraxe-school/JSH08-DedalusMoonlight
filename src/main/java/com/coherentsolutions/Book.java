6102e5e1b57912f1fa278be1c529519ae9055c18
dedalus <tim.tagil@mail.ru>
2024-09-03 15:39:08 +0300

    Created a Java program that simulates a simple book catalog

--- src/main/java/com/coherentsolutions/Book.java
+++ src/main/java/com/coherentsolutions/Book.java
@@ -2,10 +2,45 @@ package com.coherentsolutions;
 
 public class Book {
     // Declare fields for title, author, yearOfPublication, and rating (for bonus task)
-
+    private String title;
+    private String author;
+    private int yearOfPublication;
+    private int rating;
     // Implement a constructor
+
+    public Book(String title, String author, int yearOfPublication, int rating) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+        this.rating = rating;
+    }
 
     // Implement getter methods
 
-    // Implement a toString method
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYearOfPublication() {
+        return yearOfPublication;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+// Implement a toString method
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year of publication=" + yearOfPublication +
+                '}';
+    }
 }
--- src/main/java/com/coherentsolutions/HomeworkMain.java
+++ src/main/java/com/coherentsolutions/HomeworkMain.java
@@ -3,31 +3,36 @@ package com.coherentsolutions;
 public class HomeworkMain {
     public static void main(String[] args) {
         // Create a new book catalog
-        // BookCatalog catalog = new BookCatalog();
+        BookCatalog catalog = new BookCatalog();
 
         // Add some books to the catalog
-        // ...
+        catalog.addBook(new Book("Narnia", "Alex", 1999, 87));
+        catalog.addBook(new Book("London", "Harry", 1896, 46));
+        catalog.addBook(new Book("Hogwarts", "Rowling", 2003, 90));
+        catalog.addBook(new Book("Archmage", "Rudazov", 2010, 96));
+        catalog.addBook(new Book("Spider man", "Marvel", 2000, 81));
+        catalog.addBook(new Book("Narnia2", "Alex", 2001, 71));
 
         // Sort books by title
-        // ...
+        catalog.sortBooksByTitle();
 
         // Display sorted books
-        // ...
+        System.out.println(catalog.getBooks());
 
         // Find books by author
-        // ...
+        System.out.println(catalog.findBooksByAuthor("Alex"));
 
         // Remove a book by title
-        // ...
+        catalog.removeBookByTitle("London");
 
         // Display remaining books
-        // ...
+        System.out.println(catalog.getBooks());
 
         // Bonus: Calculate the average rating for books by a particular author
-        // ...
+        System.out.println(catalog.averageRatingByAuthor("Alex"));
 
         // Bonus: Get a list of all tags across all books
-        // ...
+        System.out.println(catalog.allTags());
     }
 }
 
